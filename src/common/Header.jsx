d2149af591c3f3831e08a48894b5fef39e411e3f
Hasan Ujjaman Jibon <108232029+HasanUjjamanJibon@users.noreply.github.com>
2023-05-17 14:34:51 +0600

    add server link & readme file

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="my-10 text-center">
+    <div className="my-10 text-center px-5">
       <p className="capitalize font-semibold text-2xl  bg-yellow-900 px-6 py-3 text-gray-100 rounded-md inline-block">
         Chocolate management System
       </p>
